6ffa9ff90dc5b2c95cdc9da79141a7391908849b
abhishek36 <akamble9913@gmail.com>
2022-02-08 07:41:22 +0530

    first commit

---
+++ src/controllers/episodes.controller.js
@@ -0,0 +1,30 @@
+//import Episodes model here
+
+const addEpisodes = async (req, res) => {
+  //addEpisodes api logic here
+};
+
+const getAllEpisodesByPodcastId = async (req, res) => {
+  //getAllEpisodesByPodcastId api logic here
+};
+
+const getEpisodesById = async (req, res) => {
+  //getEpisodesById api logic here
+};
+
+const updateEpisodesById = async (req, res) => {
+  //updateEpisodesById api logic here
+};
+
+const deleteEpisodesById = async (req, res) => {
+  //deleteEpisodesById api logic here
+};
+
+const EpisodesController = {
+  addEpisodes,
+  getAllEpisodesByPodcastId,
+  getEpisodesById,
+  updateEpisodesById,
+  deleteEpisodesById,
+};
+module.exports = EpisodesController;
---
+++ src/controllers/podcasts.controller.js
@@ -0,0 +1,30 @@
+//import Podcasts model here
+
+const addPodcasts = async (req, res) => {
+  //addPodcasts api logic here
+};
+
+const getAllPodcastsByCategoryId = async (req, res) => {
+  //getAllPodcastsByCategoryId api logic here
+};
+
+const getPodcastsById = async (req, res) => {
+  //getPodcastsById api logic here
+};
+
+const updatePodcastsById = async (req, res) => {
+  //updatePodcastsById api logic here
+};
+
+const deletePodcastsById = async (req, res) => {
+  //deletePodcastsById api logic here
+};
+
+const PodcastsController = {
+  addPodcasts,
+  getAllPodcastsByCategoryId,
+  getPodcastsById,
+  updatePodcastsById,
+  deletePodcastsById,
+};
+module.exports = PodcastsController;
